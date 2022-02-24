61ad4eeb2f2476af92ebbd8a71b82c5dc635881f
Adet <adetia.marhadi>
2022-02-24 08:57:54 +0700

    add post request body

---
+++ src/main/java/com/github/adetiamarhadi/productservice/rest/CreateProductRestModel.java
@@ -0,0 +1,13 @@
+package com.github.adetiamarhadi.productservice.rest;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class CreateProductRestModel {
+
+	private String title;
+	private BigDecimal price;
+	private int quantity;
+}
