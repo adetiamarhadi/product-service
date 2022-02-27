0b78f0d583c366b3c6fa57d57efd618295f1ad81
Adet <adetia.marhadi>
2022-02-27 23:13:38 +0700

    product aggregate - create class aggregate

---
+++ src/main/java/com/github/adetiamarhadi/productservice/command/ProductAggregate.java
@@ -0,0 +1,13 @@
+package com.github.adetiamarhadi.productservice.command;
+
+import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.spring.stereotype.Aggregate;
+
+@Aggregate
+public class ProductAggregate {
+
+	public ProductAggregate() {}
+
+	@CommandHandler
+	public ProductAggregate(CreateProductCommand createProductCommand) {}
+}
