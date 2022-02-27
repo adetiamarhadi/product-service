725033cbb18b199f708d9cf4ebb31b391b5172cd
Adet <adetia.marhadi>
2022-02-27 23:16:51 +0700

    product aggregate - validate create product command

--- src/main/java/com/github/adetiamarhadi/productservice/command/ProductAggregate.java
+++ src/main/java/com/github/adetiamarhadi/productservice/command/ProductAggregate.java
@@ -3,11 +3,23 @@ package com.github.adetiamarhadi.productservice.command;
 import org.axonframework.commandhandling.CommandHandler;
 import org.axonframework.spring.stereotype.Aggregate;
 
+import java.math.BigDecimal;
+
 @Aggregate
 public class ProductAggregate {
 
 	public ProductAggregate() {}
 
 	@CommandHandler
-	public ProductAggregate(CreateProductCommand createProductCommand) {}
+	public ProductAggregate(CreateProductCommand createProductCommand) {
+		// validate create product command
+		if (createProductCommand.getPrice().compareTo(BigDecimal.ZERO) <= 0) {
+			throw new IllegalArgumentException("price cannot be less or equal than zero");
+		}
+
+		if (createProductCommand.getTitle() == null
+				|| createProductCommand.getTitle().isBlank()) {
+			throw new IllegalArgumentException("title cannot be empty");
+		}
+	}
 }
